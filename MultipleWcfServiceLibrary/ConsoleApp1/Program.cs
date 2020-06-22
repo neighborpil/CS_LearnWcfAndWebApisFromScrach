86405770c2e2fc20d5c4ce9932495580a17b8be3
Neighborpil <neighborpil@naver.com>
2020-06-22 18:40:40 +0900

    mex

--- MultipleWcfServiceLibrary/ConsoleApp1/Program.cs
+++ MultipleWcfServiceLibrary/ConsoleApp1/Program.cs
@@ -14,7 +14,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Uri httpBaseAddress = new Uri("http://localhost:50010");
+            //Uri httpBaseAddress = new Uri("http://localhost:50010");
+            Uri httpBaseAddress = new Uri("http://localhost:50010/MyHttpEndPoint");
             Uri tcpBaseAddress = new Uri("net.tcp://localhost:50011");
             var serviceHost = new ServiceHost(typeof(MultipleService), new Uri[]{ tcpBaseAddress , httpBaseAddress});
 
@@ -26,9 +27,25 @@ namespace ConsoleApp1
             var httpServiceEndpoint =
                 serviceHost.AddServiceEndpoint(typeof(IMultipleService), new BasicHttpBinding(), httpBaseAddress);
 
+            // 만약 binding을 등록하지 않으면 BasicHttpBinding이 된다
+
+            /*
             var serviceMetadataBehavior = new ServiceMetadataBehavior();
-            serviceMetadataBehavior.HttpGetEnabled = true;
+            serviceMetadataBehavior.HttpGetEnabled = true; // true : Human readable format, false: Machine readable format
             serviceHost.Description.Behaviors.Add(serviceMetadataBehavior);
+            */
+            // 만약 httpGetEnabled가 false일 경우
+            var serviceMetadataBehavior = new ServiceMetadataBehavior();
+            serviceMetadataBehavior.HttpGetEnabled = false; // true : Human readable format, false: Machine readable format
+            serviceHost.Description.Behaviors.Add(serviceMetadataBehavior);
+
+            var httpServiceEndpointMex = serviceHost.AddServiceEndpoint(typeof(IMetadataExchange),
+                MetadataExchangeBindings.CreateMexHttpBinding(), "http://localhost:50010/MyHttpEndPoint/mex");
+
+
+            var tcpServiceEndpointMex = serviceHost.AddServiceEndpoint(typeof(IMetadataExchange),
+                MetadataExchangeBindings.CreateMexTcpBinding(), "net.tcp://localhost:50011/mex");
+
 
             serviceHost.Open(); // 실행 위해 관리자 모드 필요
 
