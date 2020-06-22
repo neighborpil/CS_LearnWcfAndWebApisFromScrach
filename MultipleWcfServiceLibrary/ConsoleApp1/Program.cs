55599a131b4875f7482c31eb1a17775e5cf982ed
Neighborpil <neighborpil@naver.com>
2020-06-22 13:07:05 +0900

    self hosting on console

--- MultipleWcfServiceLibrary/ConsoleApp1/Program.cs
+++ MultipleWcfServiceLibrary/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
@@ -13,20 +14,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Uri baseAddress = new Uri("http://localhost:50010");
-            var serviceHost = new ServiceHost(typeof(MultipleService), baseAddress);
+            Uri httpBaseAddress = new Uri("http://localhost:50010");
+            Uri tcpBaseAddress = new Uri("net.tcp://localhost:50011");
+            var serviceHost = new ServiceHost(typeof(MultipleService), new Uri[]{ tcpBaseAddress , httpBaseAddress});
 
             // 파라미터(CBA순이다. Contract, Binding, Address)
-            var serviceEndpoint =
-                serviceHost.AddServiceEndpoint(typeof(IMultipleService), new WSHttpBinding(), baseAddress);
+            //var serviceEndpoint =
+            //    serviceHost.AddServiceEndpoint(typeof(IMultipleService), new WSHttpBinding(), baseAddress);
+            var tcpServiceEndpoint =
+                serviceHost.AddServiceEndpoint(typeof(IMultipleService), new NetTcpBinding(), tcpBaseAddress);
+            var httpServiceEndpoint =
+                serviceHost.AddServiceEndpoint(typeof(IMultipleService), new BasicHttpBinding(), httpBaseAddress);
 
             var serviceMetadataBehavior = new ServiceMetadataBehavior();
             serviceMetadataBehavior.HttpGetEnabled = true;
             serviceHost.Description.Behaviors.Add(serviceMetadataBehavior);
 
-            serviceHost.Open();
+            serviceHost.Open(); // 실행 위해 관리자 모드 필요
 
             Console.WriteLine("Started");
+            foreach (var endpoint in serviceHost.Description.Endpoints)
+            {
+                Console.WriteLine($"Address: {endpoint.Address}");
+                Console.WriteLine($"Binding: {endpoint.Binding.Name}");
+                Console.WriteLine($"Contract: {endpoint.Contract.Name}");
+            }
+
             Console.ReadLine();
 
             serviceHost.Close();
