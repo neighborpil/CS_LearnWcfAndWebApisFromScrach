63230c7a7aaedd75a246c66c01536c2ec9204949
Neighborpil <neighborpil@naver.com>
2020-07-09 14:45:33 +0900

    save

--- MulServiceLibrary/ConsoleHost/Program.cs
+++ MulServiceLibrary/ConsoleHost/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 using MulServiceLibrary;
@@ -18,12 +19,21 @@ namespace ConsoleHost
             Uri httpBaseAddress = new Uri("http://localhost:50010/MyHttpEndPoint");
 
             var serviceHost = new ServiceHost(typeof(MulService), httpBaseAddress);
+
+            var basicHttpBinding = new BasicHttpBinding();
+            basicHttpBinding.OpenTimeout = new TimeSpan(0, 10, 0);
+            basicHttpBinding.CloseTimeout = new TimeSpan(0, 10, 0);
+
             var serviceEndPoint =
-                serviceHost.AddServiceEndpoint(typeof(IMulService), new BasicHttpBinding(), httpBaseAddress);
-            
+                serviceHost.AddServiceEndpoint(typeof(IMulService), basicHttpBinding, httpBaseAddress);
+
+            var serviceMetadataBehavior = new ServiceMetadataBehavior();
+            serviceMetadataBehavior.HttpGetEnabled = true;
+            serviceHost.Description.Behaviors.Add(serviceMetadataBehavior);
+
             serviceHost.Open();
 
-            Console.WriteLine("Started...");
+            Console.WriteLine("Started..."); 
 
             foreach (var endpoint in serviceHost.Description.Endpoints)
             {
@@ -31,9 +41,11 @@ namespace ConsoleHost
                 Console.WriteLine($"Address: {endpoint.Address}");
                 Console.WriteLine($"Binding: {endpoint.Binding.Name}");
                 Console.WriteLine($"Contract: {endpoint.Contract.Name}");
+                Console.WriteLine($"OpenTimeout: {endpoint.Binding.OpenTimeout}");
+                Console.WriteLine($"CloseTimeout: {endpoint.Binding.CloseTimeout}");
             }
 
-            Console.ReadLine();
+            Console.ReadKey();
 
             serviceHost.Close();
         }
