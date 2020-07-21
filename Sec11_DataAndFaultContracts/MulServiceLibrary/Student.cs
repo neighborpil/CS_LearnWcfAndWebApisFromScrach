8eb479eb29b1c1089ea6f3c4b4f5eb416f3858ae
Neighborpil <neighborpil@naver.com>
2020-07-21 13:02:58 +0900

    data contract testing

--- Sec11_DataAndFaultContracts/MulServiceLibrary/Student.cs
+++ Sec11_DataAndFaultContracts/MulServiceLibrary/Student.cs
@@ -5,12 +5,16 @@ namespace MulServiceLibrary
     [DataContract]
     public class Student
     {
-        // 기본 자료형의 경우 [DataMember]을 삭제하여도 정상적으로 직렬화가 일어난다
+        // 만약 특정 변수만 Service에서 표시하고 싶다면 [DataContract]와 [DataMember]를 특정 항목에만 표시하면 된다.
+        //[DataMember(Name = "Sid")]
         [DataMember]
         public int StudentId { get; set; }
 
-        [DataMember]
+        // IsRequired가 붙으면 클라이언트에서 데이터를 넘기든지 안 넘기든지 필요함(Contract를 갱신하지 않으면 예외발생)
+        [DataMember(IsRequired = true)]
         public string StudentName { get; set; }
+
+        public string StudentContent { get; set; }
 
         public Student(int studentId, string studentName)
         {
