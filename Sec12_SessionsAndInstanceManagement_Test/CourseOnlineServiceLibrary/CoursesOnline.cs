d829ba4cade58f39a724070c80fea6cbdcdc11c3
Neighborpil <neighborpil@naver.com>
2020-07-28 14:27:14 +0900

    save

--- Sec12_SessionsAndInstanceManagement_Test/CourseOnlineServiceLibrary/CoursesOnline.cs
+++ Sec12_SessionsAndInstanceManagement_Test/CourseOnlineServiceLibrary/CoursesOnline.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Diagnostics.Eventing.Reader;
 using System.IO;
-using SQLite;
 
 namespace CourseOnlineServiceLibrary
 {
     public class CoursesOnline : ICoursesOnline
     {
-        private readonly string _dbFile = Path.Combine(Environment.CurrentDirectory, "CoursesOnline.db3");
+        //private readonly string _dbFile = Path.Combine(Environment.CurrentDirectory, "CoursesOnline.sqlite");
 
         private Course course = null;
         private List<Course> courses = null;
@@ -19,31 +20,54 @@ namespace CourseOnlineServiceLibrary
             //CreateTable();
         }
 
+
         public void CreateTable()
         {
-            var dbFile = new FileInfo(_dbFile);
-            if (!dbFile.Exists)
+            using (var connection = new SQLiteConnection($"Data Source={$"D:\test.sqlite"}"))
             {
+
                 try
                 {
-                    using (var connection = new SQLiteConnection(_dbFile))
-                    {
-                        string query =
-                            "create table course(course_id int primary key auto_increment, course_name varchar(50), course_price double)";
+                    connection.Open();
 
-                        var result = connection.CreateTable<Course>();
-                        if(result == CreateTableResult.Created)
-                        {
-
-                        }
-                    }
+                    var sql =
+                        "create table course(course_id int primary key auto_increment, course_name varchar(50), course_price double)";
+                    var command = new SQLiteCommand(sql, connection);
+                    command.ExecuteNonQuery();
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
+
         }
+
+        //public void CreateTable()
+        //{
+        //    var dbFile = new FileInfo(_dbFile);
+        //    if (!dbFile.Exists)
+        //    {
+        //        try
+        //        {
+        //            using (var connection = new SQLiteConnection(_dbFile))
+        //            {
+        //                string query =
+        //                    "create table course(course_id int primary key auto_increment, course_name varchar(50), course_price double)";
+
+        //                var result = connection.CreateTable<Course>();
+        //                if(result == CreateTableResult.Created)
+        //                {
+
+        //                }
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+        //    }
+        //}
 
 
         public List<Course> ListCourses()
