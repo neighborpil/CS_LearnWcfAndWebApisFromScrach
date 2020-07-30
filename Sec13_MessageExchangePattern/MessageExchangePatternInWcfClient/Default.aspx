d1f6b9d07a4e44d62832d95305adddc546ae7ef1
Neighborpil <neighborpil@naver.com>
2020-07-31 08:37:06 +0900

    save

---
+++ Sec13_MessageExchangePattern/MessageExchangePatternInWcfClient/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="btnSignUp" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
--- Sec13_MessageExchangePattern/MessageExchangePatternInWcfConsoleHost/Program.cs
+++ Sec13_MessageExchangePattern/MessageExchangePatternInWcfConsoleHost/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using MessageExchangePatternInWcfLibrary;
 
 namespace MessageExchangePatternInWcfConsoleHost
 {
@@ -10,6 +13,33 @@ namespace MessageExchangePatternInWcfConsoleHost
     {
         static void Main(string[] args)
         {
+            var httpBaseAddress = new Uri("http://localhost:50010/");
+
+            var serviceHost = new ServiceHost(typeof(MessageExchangePattern), new Uri[] {httpBaseAddress});
+            var serviceEndpoint =
+                serviceHost.AddServiceEndpoint(typeof(IMessageExchangePattern), new BasicHttpBinding(), httpBaseAddress);
+
+            var serviceMetadataBehavior = new ServiceMetadataBehavior();
+            serviceHost.Description.Behaviors.Add(serviceMetadataBehavior);
+
+            var tcpServiceEndpointMex = serviceHost.AddServiceEndpoint(typeof(IMetadataExchange),
+                MetadataExchangeBindings.CreateMexTcpBinding(), "http://localhost:50010/mex");
+
+            serviceHost.Open();
+
+            Console.WriteLine("[WCF Started]");
+
+            foreach (var endpoint in serviceHost.Description.Endpoints)
+            {
+                Console.WriteLine($"Address: {endpoint.Address}");
+                Console.WriteLine($"Binding: {endpoint.Binding.Name}");
+                Console.WriteLine($"Contract: {endpoint.Contract.Name}");
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("[WCF Ended]");
+            serviceHost.Close();
         }
     }
 }
